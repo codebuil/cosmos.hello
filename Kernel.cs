67d068094f5a1cc34e1cc5e7c82198c173004bb1
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-12-30 18:05:49 +0000

    Add files via upload

---
+++ Kernel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace sums
+{
+    public class Kernel : Sys.Kernel
+    {
+        Double d = 0.0;
+
+        protected override void BeforeRun()
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Clear();
+            Console.WriteLine("sum calculator...");
+            
+        }
+
+        protected override void Run()
+        {
+            Console.Write("to Sum: ");
+            var input = Console.ReadLine();
+            try
+            {
+                d = d + Double.Parse(input.ToString());
+                Console.WriteLine(d);
+            }
+            catch (Exception e)
+            {
+                d = 0.00;
+                Console.WriteLine(d);
+            }
+            
+        }
+    }
+}
