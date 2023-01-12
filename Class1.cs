0fd231aa22c502063003999e6ff03df0b9baae17
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-01-12 10:26:36 +0000

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace math
+{
+    internal class mmath
+    {
+        public Double adds(Double d,Double dd)
+        {
+            Double ddddd = d + dd;
+            Console.WriteLine(ddddd);
+            return ddddd;
+        }
+    }
+}
--- Kernel.cs
+++ Kernel.cs
@@ -3,35 +3,38 @@ using System.Collections.Generic;
 using System.Text;
 using Sys = Cosmos.System;
 
-namespace sums
+namespace math
 {
     public class Kernel : Sys.Kernel
     {
-        Double d = 0.0;
-
+        Double ddd = 0.00;
+        mmath m = new mmath();
+        Double dddd = 0.00;
         protected override void BeforeRun()
         {
             Console.BackgroundColor = ConsoleColor.Green;
             Console.Clear();
-            Console.WriteLine("sum calculator...");
-            
+            Console.WriteLine("add math...");
         }
 
         protected override void Run()
         {
-            Console.Write("to Sum: ");
+            Console.Write("Input: ");
             var input = Console.ReadLine();
+            Console.Write("Text typed: ");
+            dddd = 0.00;
             try
             {
-                d = d + Double.Parse(input.ToString());
-                Console.WriteLine(d);
+
+                dddd= Double.Parse(input);
+
             }
             catch (Exception e)
             {
-                d = 0.00;
-                Console.WriteLine(d);
+                dddd = 0.00;
             }
-            
+            ddd = m.adds(ddd , dddd);
+            Console.WriteLine();
         }
     }
 }
